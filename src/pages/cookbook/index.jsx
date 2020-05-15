ac4badfd6f3f0a8194e05666662cfc5e130f0673
Mrc <manru.chen@thoughtworks.com>
2020-05-15 10:22:37 +0800

    [manru.chen] fix: cookbood image tag

--- src/pages/cookbook/index.jsx
+++ src/pages/cookbook/index.jsx
@@ -18,15 +18,16 @@ function CookBook() {
         title: '小朋友可以一口吃掉4个的杯子蛋糕',
         name: '爱做饭爱洗碗的瘦子',
         description: '零难度的小点心，没事就想折腾起来！关键是小朋友爱吃(^.^)',
+        mainImageUrl:'http://m.qpic.cn/psc?/V11rIwqj3eAlyi/U.M5PgtxS1PFgLr9JTiFCzTX36Cvg2DbyvYlj679c9GeMygzpnyI1DqDVmgmt74e99Br.hxBPXKzyrr5dA.KZg!!/b&bo=ZQR9AwAAAAARBy8!&rf=viewer_4',
         authorUrl:'http://b93.photo.store.qq.com/psb?/V11rIwqj3eAlyi/XU8KGhBuVSzzV9.QhmljsHwa5ODdix557G.XATvDMUE!/b/YXoFgTcIYwAAYmr*gDfuYgAA&bo=*wFWAQAAAAABBIo!&rf=viewer_4'
     }
+
     const [foodMaterials] = useState(mockData)
 
     const renderFoodMaterialHeader = (foodMaterialsHeaderData) => (
         <div>
             <div className="cookbook-image--box">
-                <wx-image mode="aspectFill" className="cookbook--image"
-                          src="http://m.qpic.cn/psc?/V11rIwqj3eAlyi/U.M5PgtxS1PFgLr9JTiFCzTX36Cvg2DbyvYlj679c9GeMygzpnyI1DqDVmgmt74e99Br.hxBPXKzyrr5dA.KZg!!/b&bo=ZQR9AwAAAAARBy8!&rf=viewer_4"></wx-image>
+                <wx-image mode="aspectFill" className="cookbook--image" src={foodMaterialsHeaderData.mainImageUrl}/>
             </div>
             <Layout className="menu--box">
                 <wx-p className="cookbook--title">{foodMaterialsHeaderData.title}</wx-p>
@@ -37,7 +38,7 @@ function CookBook() {
         <div>
             <Layout hasSider className="author-info--box">
                 <Sider width="60px">
-                    <wx-image className="author--icon" src={foodMaterialsHeaderData.authorUrl}></wx-image>
+                    <wx-image className="author--icon" src={foodMaterialsHeaderData.authorUrl}/>
                 </Sider>
                 <Content className="author--name">
                     {foodMaterialsHeaderData.name}
