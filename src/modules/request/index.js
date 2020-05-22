6b4b1fd765ea19f86b7de0142b55ff7d48a24f9c
benli <ben.li@thoughtworks.com>
2020-05-22 16:15:19 +0800

    process path query

--- src/modules/request/index.js
+++ src/modules/request/index.js
@@ -10,21 +10,40 @@ const axios = Axios.create({
     validateStatus: (status) => (status >= 200 && status < 300) || status === 401,
 });
 
+const getParamsWithoutPathQuery = (url, data) => {
+    const pathQueryRegxp = /{([^{}]+)}/g;
+    const matches = url.match(pathQueryRegxp);
+    if (!matches) return data;
+    return Object.keys(data).reduce((params, key) => {
+        if (!matches.includes(`{${key}}`)) {
+            return {
+                ...params,
+                [key]: data[key],
+            };
+        }
+        return params;
+    }, {});
+}
+
 export default async (serviceName, { data = {} } = {}) => {
     const { url, method } = getService(serviceName) || {};
     if (!url) {
         throw new Error(`没有${serviceName}服务`);
     }
 
+    const processedURL = url.replace(/{([^{}]+)}/g, (match, p1) => data[p1]);
+
+    const params = getParamsWithoutPathQuery(url, data);
+
     const config = {
-        url,
+        url: processedURL,
         method,
     }
 
     if (method === 'GET') {
-        config.params = data;
+        config.params = params;
     } else {
-        config.data = data;
+        config.data = params;
     }
 
     const res = await axios.request(config);
