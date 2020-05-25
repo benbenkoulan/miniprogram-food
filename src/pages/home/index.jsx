0c203f0f17abdba4345e0bce9df50c8d5a32da2c
Mrc <manru.chen@thoughtworks.com>
2020-05-25 12:46:30 +0800

    [manru.chen] feat: index

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,28 +1,151 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { send } from '~/modules/request/proxy'
 
-import { settingSelector } from '../../store/selector';
-import router from '../../router';
+import { settingSelector } from '../../store/selector'
+import router from '../../router'
 
-import './style.css';
+import './style.css'
+import { BASE_REQUEST_URL } from '../../modules/constant/network'
 
 function Index() {
-    const settings = useSelector(settingSelector);
+    const scrollViewRef = useRef()
+
+    const settings = useSelector(settingSelector)
+    const [searchQuery, setSearchQuery] = useState({
+        pageNumber: 0,
+        pageSize: 6
+    })
+    const [cookbookList, setCookBookList] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [hasMore, setHasMore] = useState(true)
 
     useEffect(() => {
         // console.log('settings: ', settings);
-    }, [settings]);
+    }, [settings])
+
+    useEffect(() => {
+        setIsLoading(true)
+        const fetchData = async () => {
+            try {
+                const { content = [] } = await send('searchCookbooks', { data: searchQuery })
+                console.log(content)
+                const cookbooks = content
+                if (searchQuery.pageNumber === 0) {
+                    setCookBookList(cookbooks)
+                } else {
+                    setCookBookList((cookbookList) => ([
+                        ...cookbookList,
+                        ...cookbooks
+                    ]))
+                }
+                if (cookbooks.length < searchQuery.pageSize) {
+                    setHasMore(false)
+                }
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        fetchData()
+    }, [searchQuery])
+
+
+    const loadMore = () => {
+        console.log('loadMore')
+        setSearchQuery({
+            ...searchQuery,
+            pageNumber: searchQuery.pageNumber + 1
+        })
+    }
+
+    useEffect(() => {
+        const handleScrollToLower = () => {
+            console.log('!isLoading && hasMore', hasMore)
+            if (!isLoading && hasMore) {
+                loadMore()
+            }
+        }
+        scrollViewRef.current.addEventListener('scrolltolower', handleScrollToLower)
+        return () => {
+            scrollViewRef.current.removeEventListener('scrolltolower', handleScrollToLower)
+        }
+    }, [isLoading, hasMore, loadMore])
 
     const handleSearchLink = () => {
-        console.log(settings);
-        router.push('search');
+        console.log(settings)
+        router.push('search')
+    }
+
+    const data = {
+        imgWidth: 0, imgHeight: 0,
+        note: [
+            {
+                title: '案例名称',
+                url: '/assets/images/logo_text.jpg'
+            },
+            {
+                title: '你所不知道的红酒知识',
+                url: '/assets/images/logo.jpg'
+            },
+            {
+                title: '红酒知识',
+                url: '/assets/images/logo_text.jpg'
+            },
+            {
+                title: 'asdasd',
+                url: '/assets/images/logo.jpg'
+            },
+            {
+                title: 'dfgdfg',
+                url: '/assets/images/logo_text.jpg'
+            },
+
+            {
+                title: 'erter',
+                url: '/assets/images/logo.jpg'
+            },
+            {
+                title: '红酒知识',
+                url: '/assets/images/logo_text.jpg'
+            },
+            {
+                title: 'asdasd',
+                url: '/assets/images/logo.jpg'
+            },
+            {
+                title: 'dfgdfg',
+                url: '/assets/images/logo_text.jpg'
+            },
+
+            {
+                title: 'erter',
+                url: '/assets/images/logo.jpg'
+            }
+        ]
     }
 
     return (
-        <div className="page home--page">
-            <div className="search--link" onClick={handleSearchLink}>今天想吃点什么?</div>
-        </div>
-    );
+        <wx-scroll-view ref={scrollViewRef} lower-threshold="100" scroll-y={true} style={{ height:'100%'}}>
+            <div className="home--page">
+                <div className="logo-box--wrap">
+                    <div className="logo--box">
+                        <wx-image src="/assets/images/logo_text.jpg" className="logo--text"/>
+                        <wx-image src="/assets/images/logo.jpg" className="logo"/>
+                    </div>
+                </div>
+                <div className="search--link" onClick={handleSearchLink}>今天想吃点什么?</div>
+                <p className="description">不一样的美食秘籍</p>
+            </div>
+            <wx-view className='list-masonry'>
+                {cookbookList && cookbookList.map(cook => (
+                    <wx-view className='item-masonry'>
+                        <wx-image src={`${BASE_REQUEST_URL}/services/file/images/${cook.mainImageId}`} mode='widthFix'/>
+                        <wx-text>{cook.title}</wx-text>
+                    </wx-view>
+                ))}
+            </wx-view>
+        </wx-scroll-view>
+    )
 }
 
-export default Index;
+export default Index
