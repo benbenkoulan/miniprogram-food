6551d8a741c7813c1b966cdf8d319f2e3748c455
Mrc <manru.chen@thoughtworks.com>
2020-06-09 12:19:18 +0800

    [manru.chen] fix: bug iphone no pull down

--- src/pages/search/components/sortContainer/index.jsx
+++ src/pages/search/components/sortContainer/index.jsx
@@ -14,17 +14,19 @@ function SortContainer(props) {
     const getCheckSortTypeHandler = (sortType) => () => onCheck(sortType);
 
     return (
-        <Row className="sort--container" gutter={10}>
-            <Col className={ currentSortKey === SORT_TYPE.DEFAULT ? 'sort--box active' : 'sort--box' } span={8} onClick={getCheckSortTypeHandler(SORT_TYPE.DEFAULT)}>
-                综合
-            </Col>
-            <Col className={ currentSortKey === SORT_TYPE.COLLECTION ? 'sort--box active' : 'sort--box' } span={8} onClick={getCheckSortTypeHandler(SORT_TYPE.COLLECTION)}>
-                收藏
-            </Col>
-            <Col className={ currentSortKey === SORT_TYPE.TIMELINE ? 'sort--box active' : 'sort--box' } span={8} onClick={getCheckSortTypeHandler(SORT_TYPE.TIMELINE)}>
-                最新发布
-            </Col>
-        </Row>
+        <div>
+            <Row className="sort--container" gutter={10}>
+                <Col className={ currentSortKey === SORT_TYPE.DEFAULT ? 'sort--box active' : 'sort--box' } span={8} onClick={getCheckSortTypeHandler(SORT_TYPE.DEFAULT)}>
+                    综合
+                </Col>
+                <Col className={ currentSortKey === SORT_TYPE.COLLECTION ? 'sort--box active' : 'sort--box' } span={8} onClick={getCheckSortTypeHandler(SORT_TYPE.COLLECTION)}>
+                    收藏
+                </Col>
+                <Col className={ currentSortKey === SORT_TYPE.TIMELINE ? 'sort--box active' : 'sort--box' } span={8} onClick={getCheckSortTypeHandler(SORT_TYPE.TIMELINE)}>
+                    最新发布
+                </Col>
+            </Row>
+        </div>
     )
 }
 
